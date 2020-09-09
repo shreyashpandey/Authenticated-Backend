570c512201129b08ff3467c1607111a7dd5dd649
Shreyansh Pandey <shreyansh.p@lsq.net>
2020-09-09 10:27:59 +0530

    My Backend Project

---
+++ config/database.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://localhost/node_rest_api';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+module.exports = mongoose;
